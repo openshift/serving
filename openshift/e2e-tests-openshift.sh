6b30aa9a14a69d9c8563b2d79b9b7dd5bfcee480
Ben Browning <bbrownin@redhat.com>
2019-02-26 13:50:22 -0500

    Scale up the number of workers before running tests

--- openshift/e2e-tests-openshift.sh
+++ openshift/e2e-tests-openshift.sh
@@ -46,6 +46,35 @@ Host *
 EOF
 }
 
+function scale_up_workers(){
+  local cluster_api_ns="openshift-machine-api"
+  # Get the name of the first machineset that has at least 1 replica
+  local machineset=$(oc get machineset -n ${cluster_api_ns} -o custom-columns="name:{.metadata.name},replicas:{.spec.replicas}" -l sigs.k8s.io/cluster-api-machine-role=worker | grep " 1" | head -n 1 | awk '{print $1}')
+  # Bump the number of replicas to 4
+  oc patch machineset -n ${cluster_api_ns} ${machineset} -p '{"spec":{"replicas":4}}' --type=merge
+  wait_until_machineset_scales_up ${cluster_api_ns} ${machineset} 4
+}
+
+# Waits until the machineset in the given namespaces scales up to the
+# desired number of replicas
+# Parameters: $1 - namespace
+#             $2 - machineset name
+#             $3 - desired number of replicas
+function wait_until_machineset_scales_up() {
+  echo -n "Waiting until machineset $2 in namespace $1 scales up to $3 replicas"
+  for i in {1..150}; do  # timeout after 15 minutes
+    local available=$(oc get machineset -n $1 $2 -o jsonpath="{.status.availableReplicas}")
+    if [[ ${available} -eq $3 ]]; then
+      echo -e "\nMachineSet $2 in namespace $1 successfully scaled up to $3 replicas"
+      return 0
+    fi
+    echo -n "."
+    sleep 6
+  done
+  echo - "\n\nError: timeout waiting for machineset $2 in namespace $1 to scale up to $3 replicas"
+  return 1
+}
+
 # Waits until the given hostname resolves via DNS
 # Parameters: $1 - hostname
 function wait_until_hostname_resolves() {
@@ -268,6 +297,8 @@ function tag_built_image() {
 if [[ $ENABLE_ADMISSION_WEBHOOKS == "true" ]]; then
   enable_admission_webhooks
 fi
+
+scale_up_workers
 
 create_test_namespace
 
