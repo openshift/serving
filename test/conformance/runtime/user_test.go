a91d586391d961215d1943b3ba5854a880215fd0
Markus Thömmes <markusthoemmes@me.com>
2019-06-17 11:05:02 +0200

    :fire: Apply carried patches.

--- test/conformance/runtime/user_test.go
+++ test/conformance/runtime/user_test.go
@@ -71,6 +71,7 @@ func TestMustRunAsUser(t *testing.T) {
 // in the Dockerfile is respected when executed in Knative as declared by "SHOULD"
 // in the runtime-contract.
 func TestShouldRunAsUserContainerDefault(t *testing.T) {
+	t.Skip("Known not to work on OCP as is.")
 	t.Parallel()
 	clients := test.Setup(t)
 	_, ri, err := fetchRuntimeInfoUnprivileged(t, clients, &v1a1test.Options{})
