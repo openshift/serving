4276a11c72cb31c32dcc2aaf3151cd6d5a7ee2d2
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2020-04-14 22:20:46 +0900

    Update catalogsource and use Kourier version to v0.3.12

--- openshift/e2e-tests-openshift.sh
+++ openshift/e2e-tests-openshift.sh
@@ -95,7 +95,6 @@ function install_knative(){
   export IMAGE_webhook=${IMAGE_FORMAT//\$\{component\}/knative-serving-webhook}
   # Kourier is not built in this project.
   # export IMAGE_kourier=${IMAGE_FORMAT//\$\{component\}/kourier}
-  export IMAGE_kourier="quay.io/3scale/kourier:v0.3.11"
   envsubst < openshift/olm/knative-serving.catalogsource.yaml | oc apply -n $OLM_NAMESPACE -f -
   timeout 900 '[[ $(oc get pods -n $OLM_NAMESPACE | grep -c serverless) -eq 0 ]]' || return 1
   wait_until_pods_running $OLM_NAMESPACE
@@ -143,7 +142,7 @@ spec:
   source: ${name}
   sourceNamespace: $OLM_NAMESPACE
   name: ${name}
-  channel: preview-4.3
+  channel: 4.4
 EOF
 }
 
