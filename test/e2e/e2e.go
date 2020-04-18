d1b49ed691e613a254b713fc273acc0768a2bc77
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2020-04-18 11:01:10 +0900

    Revert "Add log why endpoint failed to get"
    This reverts commit e1005d02cb031dce95eed5c4ca70251e24970297.

--- test/e2e/e2e.go
+++ test/e2e/e2e.go
@@ -78,7 +78,7 @@ func autoscalerCM(clients *test.Clients) (*autoscalerconfig.Config, error) {
 		autoscalerconfig.ConfigName,
 		metav1.GetOptions{})
 	if err != nil {
-		return nil, fmt.Errorf("failed to get configmap %s/%s: %w", system.Namespace(), autoscalerconfig.ConfigName, err)
+		return nil, err
 	}
 	return autoscalerconfig.NewConfigFromMap(autoscalerCM.Data)
 }
@@ -120,17 +120,16 @@ func WaitForScaleToZero(t *testing.T, deploymentName string, clients *test.Clien
 
 // waitForActivatorEndpoints waits for the Service endpoints to match that of activator.
 func waitForActivatorEndpoints(resources *v1test.ResourceObjects, clients *test.Clients) error {
-	var err error
-	pollErr := wait.Poll(250*time.Millisecond, time.Minute, func() (bool, error) {
+	return wait.Poll(250*time.Millisecond, time.Minute, func() (bool, error) {
 		// We need to fetch the activator endpoints at every check, since it can change.
 		aeps, err := clients.KubeClient.Kube.CoreV1().Endpoints(
 			system.Namespace()).Get(networking.ActivatorServiceName, metav1.GetOptions{})
 		if err != nil {
-			return false, fmt.Errorf("failed to get endpoint %s/%s: %w", system.Namespace(), networking.ActivatorServiceName, err)
+			return false, nil
 		}
 		sks, err := clients.NetworkingClient.ServerlessServices.Get(resources.Revision.Name, metav1.GetOptions{})
 		if err != nil {
-			return false, fmt.Errorf("failed to get SKS %s: %w", resources.Revision.Name, err)
+			return false, nil
 		}
 
 		svcEps, err := clients.KubeClient.Kube.CoreV1().Endpoints(test.ServingNamespace).Get(
@@ -163,8 +162,4 @@ func waitForActivatorEndpoints(resources *v1test.ResourceObjects, clients *test.
 		}
 		return false, nil
 	})
-	if pollErr != nil {
-		return fmt.Errorf("failed to get endpoint: %v; %w", pollErr, err)
-	}
-	return nil
 }
