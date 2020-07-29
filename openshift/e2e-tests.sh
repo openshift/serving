56763665fce72604f913c55364fc41ddd32a7300
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2020-07-29 12:37:16 +0900

    [WIP - debug only] dump webhook on OpenShift test env

--- openshift/e2e-tests.sh
+++ openshift/e2e-tests.sh
@@ -7,6 +7,15 @@ set -x
 
 env
 
+echo "mutatingwebhookconfigurations"
+oc get mutatingwebhookconfigurations  --all-namespaces  -o yaml
+echo ""
+
+echo "validatingwebhookconfigurations"
+oc get validatingwebhookconfigurations  --all-namespaces  -o yaml
+
+exit 1
+
 scale_up_workers || exit $?
 
 failed=0
@@ -14,7 +23,6 @@ failed=0
 (( !failed )) && install_knative || failed=1
 (( !failed )) && prepare_knative_serving_tests || failed=2
 (( !failed )) && run_e2e_tests || failed=3
-(( failed )) && dump_cluster_state
 (( failed )) && exit $failed
 
 success
