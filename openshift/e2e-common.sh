b7826831ae73f7b8de78eed89db0d6c02641d28b
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-05-06 14:46:39 +0900

    mount empty file

--- openshift/e2e-common.sh
+++ openshift/e2e-common.sh
@@ -181,6 +181,8 @@ EOF
 # The mounted kourier.yaml is not used but the manifest in "knative-openshift" via KOURIER_MANIFEST_PATH is used.
 # Mounting configmap because knative-operator needs KO_DATA_PATH/ingress/0.21 directory.
 # TODO: Use manifest in this knative-operator instead of knative-openshift's KOURIER_MANIFEST_PATH.
+  touch /tmp/empty.yaml
+  oc create configmap empty-cm -n $OPERATORS_NAMESPACE --from-file="/tmp/empty.yaml" || return $?
   cat << EOF | yq write --inplace --script - $CSV || return $?
 # kourier
 - command: update
@@ -193,10 +195,10 @@ EOF
   value:
     name: "kourier-manifest"
     configMap:
-      name: "kourier-cm"
+      name: "empty-cm"
       items:
-        - key: "kourier.yaml"
-          path: "kourier.yaml"
+        - key: "empty.yaml"
+          path: "empty.yaml"
 EOF
 
 }
